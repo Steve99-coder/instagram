da9582554a396c50238cbc3973914feba9fd3d1b
Steve99-coder <stevenderitu99@gmail.com>
2021-01-17 09:44:58 +0300

    Add the view function profile

--- instaClone/views.py
+++ instaClone/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Post, Comment, Profile, Follow
+from .forms import SignUpForm, UpdateForm, UpdateProfileForm, PostForm, CommentForm
 
 # Create your views here.
 @login_required(login_url='login')
@@ -19,3 +20,24 @@ def index(request):
         form = PostForm()
     
     return render(request, 'instaClone/index.html', {'images':images, 'form': form, 'users': users})
+
+@login_required(login_url='login')
+def profile(request, username):
+    images = request.user.profile.posts.all()
+    if request.method == 'POST':
+        user_form = UpdateForm(request.POST, instance=request.user)
+        prof_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateForm(instance=request.user)
+        prof_form = UpdateProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form,
+        'images': images,
+
+    }
+    return render(request, 'instaClone/profile.html', params)
