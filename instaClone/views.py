a487beadd2591d81a4ca09c7a31962664ea187c4
Steve99-coder <stevenderitu99@gmail.com>
2021-01-17 20:20:22 +0300

    Add view function for signup

--- instaClone/views.py
+++ instaClone/views.py
@@ -181,3 +181,18 @@ class PostLikeAPIToggle(APIView):
         }
         print(data)
         return Response(data)
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
