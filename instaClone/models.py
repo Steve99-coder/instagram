6b0ee171125afd0f9fe90afcccd336157c36c784
Steve99-coder <stevenderitu99@gmail.com>
2021-01-17 08:49:52 +0300

    Add a profile model

--- instaClone/models.py
+++ instaClone/models.py
@@ -44,3 +44,33 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ["-pk"]
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='media/', default='default.png')
+    bio = models.TextField(max_length=350, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=150)
+    location = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
