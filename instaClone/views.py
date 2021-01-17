a16e28ebb2ed57fee421467ce497c287215d1d0c
Steve99-coder <stevenderitu99@gmail.com>
2021-01-17 09:37:42 +0300

    Add forms.py

--- instaClone/views.py
+++ instaClone/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Post, Comment, Profile, Follow
 
 # Create your views here.
 @login_required(login_url='login')
