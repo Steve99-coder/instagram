cae0a5c8d68a959e464faf227f39abec26cdcd8e
Steve99-coder <stevenderitu99@gmail.com>
2021-01-17 21:06:03 +0300

    Add a signup html

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Application definition
 
@@ -42,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instaClone',
+    'crispy_forms',
+    'bootstrap4',
+
+
 
 
 ]
