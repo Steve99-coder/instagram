98ae6c42455a47883d3a6f028f2bb78baf4557ea
Steve99-coder <stevenderitu99@gmail.com>
2021-01-18 14:26:32 +0300

    Add procfile

--- instaClone/urls.py
+++ instaClone/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 from .views import PostLikeToggle, PostLikeAPIToggle
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -19,3 +21,6 @@ urlpatterns = [
     path('post/<id>', views.post_comment, name='comment'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
